7c3457030dec0ed690d3c3549aec6dcbbedc0370
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-08 17:50:57 +0300

    loop lab DONE

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+  const message = [];
+  for (let i = 0; i < name.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+
+function countDown() {
+  let count = 10;
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
